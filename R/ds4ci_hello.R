917000100d80875c7a6f8909cb5acca18b954d6a
Jim Porzak <jim@ds4ci.org>
2015-02-17 19:23:12 -0800

    first binary build

--- R/ds4ci_hello.R
+++ R/ds4ci_hello.R
@@ -1,1 +1,9 @@
-hello <- function(to_who = "World") cat("Hello", to_who, "from DS4CI", "\n")
+#' Say hello to World or stated person/group.
+
+#' @param to_who string
+#' @return "Hello [World | <to_who>] from DS4CI"
+#' @examples
+#' ds4ci_hello()
+#' ds4ci_hello("Fred")
+
+ds4ci_hello <- function(to_who = "World") cat("Hello", to_who, "from DS4CI", "\n")
